a4591749cfbec5d57fa5589b67acaf7ad95c0ad5
MyAn Tran <mydanh1177@gmail.com>
2024-09-03 22:11:41 +0700

    h

--- code/index.js
+++ code/index.js
@@ -13,72 +13,70 @@ function menuToggle() {
 const buttons = document.querySelectorAll('.button');
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
-        // Your custom logic here
+
         console.log('Button clicked!');
     });
 });
-$(document).ready(function(){
+$(document).ready(function () {
     $(".up").hide()
-    $(window).scroll(function() {
-        if($(this).scrollTop() >= 100)
+    $(window).scroll(function () {
+        if ($(this).scrollTop() >= 100)
             $(".up").show()
         else
             $(".up").hide()
     })
-    $(".up").click(function(){
+    $(".up").click(function () {
         $("html,body").animate({
             scrollTop: 0
         }, 500);
     })
 })
 
-$(document).ready(function(){
+$(document).ready(function () {
     $(".product").slice(0, 4).fadeIn()
-    $(".viewall").click(function(){
-        $(".product").slice(0,12).fadeIn()
-        $(".viewall").hide()
-        $(".viewless").show()
-    })
-    $(".viewless").click(function(){
-        $(".product").hide()
-        $(".product").slice(0, 4).fadeIn()
-        $(".viewless").hide()
-        $(".viewall").show()
-    })
 });
-
-$(document).ready(function(){
+$(document).ready(function () {
+    $('.viewall').click(function () {
+        // Navigate to the all-products page
+        window.location.href = 'product.html';
+    });
+});
+$(document).ready(function () {
     $(".heart2").hide()
-    $(".heart1").click(function(){
+    $(".heart1").click(function () {
         $(".heart1").hide()
         $(".heart2").show()
     })
-    $(".heart2").click(function(){
+    $(".heart2").click(function () {
         $(".heart2").hide()
         $(".heart1").show()
     })
 })
 
 
-$(document).ready(function() {
+$(document).ready(function () {
     $(".preview").hide();
-    $(".product").click(function() {
+    $(".product").click(function () {
         $(".preview").hide();
         var productIndex = $(this).attr('class').match(/\d+/)[0];
         $(".preview" + productIndex).show();
     });
-    $(".del").click(function(){
+    $(".del").click(function () {
         $(".preview").hide()
     })
 });
 
 
-window.onload = function(){
-    let big = document.getElementById("big");
-    let images=document.querySelectorAll(".small img");
-    images.forEach(function(im) {
-        im.onclick = function() {
-          big.src = this.src;
-        };
+$(document).ready(function() {
+    $('.thumbnail').click(function() {
+        // Find the closest parent .product-detail and then find the .mainImage within that context
+        var newSrc = $(this).attr('src');
+        $(this).closest('.preview').find('.mainImage').attr('src', newSrc);
     });
-}    
+});
+$(document).ready(function () {
+    $(".success").hide();
+    $(".submit").click(function () {
+        $(".success").show()
+    })
+})
